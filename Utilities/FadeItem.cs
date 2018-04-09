506f6b35a4fc0b3a2cc8a37a88f4e35ddbf5d9e8
Unknown <markchaiyarit@icloud.com>
2018-04-09 15:48:04 +0700

    add transition screen

---
+++ Utilities/FadeItem.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP_Final_Catapult.Utilities {
+    class FadeItem {
+        public float Xpos { get; set; }
+        public float Ypos { get; set; }
+
+        public float Scale {
+            get {
+                if(Delay > 0) return 2.0f;
+                else if(Radians > MathHelper.Pi) return 0f;
+                return (float)Math.Cos(Radians) + 1;
+            }
+        }
+
+        public float Delay { get; set; }
+        public float Radians { get; set; }
+
+        public void Update(float deltaTimeInMilliseconds) {
+            Delay -= deltaTimeInMilliseconds;
+            if(Delay < 0) {
+                Radians += deltaTimeInMilliseconds / 200.0f;
+            }
+        }
+    }
+}
--- Utilities/ScreenTransitions.cs
+++ Utilities/ScreenTransitions.cs
@@ -1,18 +1,45 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using GP_Final_Catapult.Properties;
+using System.Collections.Generic;
 
 namespace GP_Final_Catapult.Utilities{
     static class ScreenTransitions {
         private static Texture2D Circle;
-
+        private static float scale;
+        static List<FadeItem> items = new List<FadeItem>(); 
         public static void SetTexture(Texture2D _texture) {
             Circle = _texture;
         }
+
+        public static void Initialize() {
+            int width = Settings.Default.ScreenWidth;
+            int height = Settings.Default.ScreenHeight;
+            for(int xPixel = 32; xPixel - 32 < width; xPixel += 64) {
+                for(int yPixel = 32; yPixel - 32 < height; yPixel += 64) {
+                    items.Add(new FadeItem() {
+                        Xpos = xPixel,
+                        Ypos = yPixel,
+                        Delay = xPixel + yPixel
+                    });
+                }
+            }
+        }
         public static void Update(GameTime gameTime) {
 
+            foreach(var item in items) {
+                item.Update((float)gameTime.ElapsedGameTime.TotalMilliseconds);
+            }
+            
+            //scale = (float)System.Math.Cos(gameTime.TotalGameTime.TotalMilliseconds / 200.0) + 1;
         }
         public static void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Begin();
+            foreach(var item in items) {
+                spriteBatch.Draw(Circle, new Vector2(item.Xpos, item.Ypos), null, Color.White, 0f, new Vector2(32, 32), item.Scale, SpriteEffects.None, 0);
 
+            }
+            spriteBatch.End();
         }
     }
 }
